586ee415a9a6e47f78468f96cb29ae3c0411ef0f
kaua sousa <kaua.sousa@catskillet.com>
2022-11-21 17:26:18 -0300

    iniciando projeto

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    })
+})
+
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080: http://localhost:8080");
 });
